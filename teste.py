f21b4f10e733c98be4b03a4f34e2ff0fcf98a011
emersonleo <emerson.leonardo@ufrpe.br>
2021-01-07 20:25:34 -0300

    Adicionando arquivos iniciais

---
+++ teste.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+casasDataset = pd.read_csv("casas_aluguel.csv")
+print(casasDataset.info())
+
+#floor                  10692 non-null object
+#animal                 10692 non-null object
+#furniture              10692 non-null object
