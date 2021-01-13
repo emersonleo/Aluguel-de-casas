25af7e59237d13ac964391cf9df11e60e39e736c
emersonleo <emerson.leonardo@ufrpe.br>
2021-01-12 22:43:43 -0300

    padronizando os valores não-numéricos

--- teste.py
+++ teste.py
@@ -1,7 +1,16 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+lb = LabelEncoder() 
 
 casasDataset = pd.read_csv("casas_aluguel.csv")
-print(casasDataset.info())
+
+casasDataset.animal = lb.fit_transform(casasDataset.animal)
+casasDataset.furniture = lb.fit_transform(casasDataset.furniture)
+casasDataset.city = lb.fit_transform(casasDataset.city)
+casasDataset.floor = lb.fit_transform(casasDataset.floor)
+
+print(casasDataset.groupby(by='city').size())
 
 #floor                  10692 non-null object
 #animal                 10692 non-null object
