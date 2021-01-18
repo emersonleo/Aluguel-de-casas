27d04534906b67f199a8ed53113ca497de638f48
emersonleo <emerson.leonardo@ufrpe.br>
2021-01-17 22:27:25 -0300

    Definindo X, Y e a função de regressão

--- teste.py
+++ teste.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import SGDRegressor
+#https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.SGDRegressor.html
+import numpy as np
 
 lb = LabelEncoder() 
 
@@ -10,7 +13,11 @@ casasDataset.furniture = lb.fit_transform(casasDataset.furniture)
 casasDataset.city = lb.fit_transform(casasDataset.city)
 casasDataset.floor = lb.fit_transform(casasDataset.floor)
 
-print(casasDataset.groupby(by='city').size())
+x = casasDataset.iloc[:, :-1]
+y = casasDataset.iloc[:, -1]
+a,b = np.polyfit(x,y,1)
+
+
 
 #floor                  10692 non-null object
 #animal                 10692 non-null object
